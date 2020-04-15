e3a3b3ea5cd68a1a3786cdd9a469cff1a6f1f944
Jano94 <60603088+Jano94@users.noreply.github.com>
2020-04-15 16:30:01 +0200

    problem with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {HashRouter , Switch , Route} from "react-router-dom/modules/HashRouter";
+import Home from "./components/home/header";
+
 
 function App() {
   return (
-    <div className="działa">
-      Lubię działający react
-    </div>
+    <>
+        <HashRouter>
+            <Switch>
+                <Route path="/home" component={Home} />
+            </Switch>
+        </HashRouter>
+        </>
   );
 }
 
---
+++ src/components/home/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => {
+  return (
+      <>
+      <p> Działa </p>
+      </>
+  )
+};
+export default Home;
