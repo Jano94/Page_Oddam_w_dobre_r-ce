bd3b83ff222cff46a3db44b39d6e1abe0c56044e
Jano94 <60603088+Jano94@users.noreply.github.com>
2020-04-21 20:32:58 +0200

    pagination + footer

---
+++ src/components/home/article_4Help/InfoArrays/LocalArray.js
@@ -0,0 +1,23 @@
+const FundacionArray= [
+    {
+        title: "Fundacja “Dbam o Zdrowie”",
+        info: "Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",
+        things: "ubrania, jedzenie, sprzęt AGD, meble, zabawki",
+        ID: 1
+    },
+    {
+        title: "Fundacja “Dla dzieci”",
+        info: "Cel i misja: Pomoc dzieciom z ubogich rodzin.",
+        things: "ubrania, meble, zabawki",
+        ID: 2
+    },
+    {
+        title: "Fundacja “Bez domu”",
+        info: "Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.",
+        things: "ubrania, jedzenie, ciepłe koce",
+        ID: 3
+    },
+];
+
+
+export default FundacionArray;
