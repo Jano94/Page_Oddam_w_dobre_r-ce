dd21ccbda417e1c6a737086036a689a1e2fc16b8
Jano94 <60603088+Jano94@users.noreply.github.com>
2020-04-29 14:45:13 +0200

    modification of .gitignore

---
+++ build/precache-manifest.dbf4f48268599e90ced5448a6141137c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb5fff59ae02a9f699369d40138bf915",
+    "url": "/index.html"
+  },
+  {
+    "revision": "201d1576f9eba223f26a",
+    "url": "/static/css/main.e6b5f853.chunk.css"
+  },
+  {
+    "revision": "43c95461f6040fff067c",
+    "url": "/static/js/2.31c2311f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.31c2311f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "201d1576f9eba223f26a",
+    "url": "/static/js/main.92525870.chunk.js"
+  },
+  {
+    "revision": "09840693bcaf6feda71c",
+    "url": "/static/js/runtime-main.39c8b874.js"
+  },
+  {
+    "revision": "99f2e620a9f4302f9861282162231969",
+    "url": "/static/media/3ColumnsBackground.99f2e620.png"
+  },
+  {
+    "revision": "9e008dd7463b8ad2609175dfb8d72fe6",
+    "url": "/static/media/cloathes.9e008dd7.png"
+  },
+  {
+    "revision": "87e72ca70656ecef0cd0617a5de6ffd3",
+    "url": "/static/media/heroIMG.87e72ca7.png"
+  },
+  {
+    "revision": "80a70b6f49e0aa104ea9400badd00572",
+    "url": "/static/media/people.80a70b6f.png"
+  },
+  {
+    "revision": "ccb98baa01b6439511b782faea0b0f1c",
+    "url": "/static/media/signature.ccb98baa.png"
+  }
+]);
