d351519657d78ae016c2ce517bfe495c98e84ff5
Jano94 <60603088+Jano94@users.noreply.github.com>
2020-05-04 21:43:16 +0200

    Updates

---
+++ precache-manifest.5d9c6f1205861a1c95227a1252973e0e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202b807d506e85e51d66a730f88064cb",
+    "url": "/portfolio_lab_page/index.html"
+  },
+  {
+    "revision": "9ccdf3b0c27474c9d927",
+    "url": "/portfolio_lab_page/static/css/main.07df503a.chunk.css"
+  },
+  {
+    "revision": "65895cc082c82bda8de2",
+    "url": "/portfolio_lab_page/static/js/2.f6faf545.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio_lab_page/static/js/2.f6faf545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ccdf3b0c27474c9d927",
+    "url": "/portfolio_lab_page/static/js/main.1c732bb5.chunk.js"
+  },
+  {
+    "revision": "c3e9e94c4e0efc0262d8",
+    "url": "/portfolio_lab_page/static/js/runtime-main.457d9af1.js"
+  },
+  {
+    "revision": "99f2e620a9f4302f9861282162231969",
+    "url": "/portfolio_lab_page/static/media/3ColumnsBackground.99f2e620.png"
+  },
+  {
+    "revision": "9e008dd7463b8ad2609175dfb8d72fe6",
+    "url": "/portfolio_lab_page/static/media/cloathes.9e008dd7.png"
+  },
+  {
+    "revision": "87e72ca70656ecef0cd0617a5de6ffd3",
+    "url": "/portfolio_lab_page/static/media/heroIMG.87e72ca7.png"
+  },
+  {
+    "revision": "80a70b6f49e0aa104ea9400badd00572",
+    "url": "/portfolio_lab_page/static/media/people.80a70b6f.png"
+  },
+  {
+    "revision": "ccb98baa01b6439511b782faea0b0f1c",
+    "url": "/portfolio_lab_page/static/media/signature.ccb98baa.png"
+  }
+]);
